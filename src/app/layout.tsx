e80e671c372ba6aee415cf39064e8539f7b2cdbd
madhavan-developer <madhavangopideveloper@gmail.com>
2026-01-13 11:28:55 +0530

    meta pixel code updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', 'YOUR_PIXEL_ID');
+              fbq('init', '2011484019016905');
               fbq('track', 'PageView');
             `,
           }}
@@ -53,7 +53,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2011484019016905&ev=PageView&noscript=1"
           />
         </noscript>
         {/* End Meta Pixel Code */}
